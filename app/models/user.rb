3dabb7b7440a5f6684269187b2dbd7376871d570
vudrok <jose@4web.mx>
2014-04-29 16:54:59 -0500

    Fixed bug with user profile validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,15 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   
   validates :first_name, presence: true
+
   validates :last_name, presence: true
-  validates :profile_name, presence: true, uniqueness: true,
-                                           format: {
-                                            with: /a-zA-Z0-9_-/,
-                                            message: 'Must be formatted correctly.'
-                                           }
+
+  validates :profile_name, presence: true, 
+                          uniqueness: true,
+                          format: {
+                            with: /\A[a-zA-Z0-9_-]+\z/,
+                            message: 'Must be formatted correctly.'
+                          }
 
   has_many :statuses
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -32,12 +32,22 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 	test "a user should have a profile name without spaces" do
-		user = User.new
+		user = User.new(first_name: 'Vladimir', last_name: 'Putin', email: 'vladimirs@putin.ru')
+		user.password = user.password_confirmation = 'password'
+		
 		user.profile_name = "My Profile With spaces"
 
 		assert !user.save
 		assert !user.errors[:profile_name].empty?
 		assert user.errors[:profile_name].include?("Must be formatted correctly.")
+	end
 
+
+	test "a user can have a correctly formatted profile name" do
+		user = User.new(first_name: 'Vladimir', last_name: 'Putin', email: 'vladimirs@putin.ru')
+		user.password = user.password_confirmation = 'password'
+
+		user.profile_name = 'vladimirsh'
+		assert user.valid?
 	end
 end
