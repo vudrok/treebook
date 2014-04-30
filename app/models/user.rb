a9e0726975ad0dd8ca0f040eb184b8003bf5bcd0
vudrok <jose@4web.mx>
2014-04-30 10:55:07 -0500

    Added gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,12 @@ class User < ActiveRecord::Base
   def full_name
   	first_name + " " +last_name
   end
+
+  def gravatar_url
+    stripped_email = email.strip
+    downcase_email = stripped_email.downcase
+    hash = Digest::MD5.hexdigest(downcase_email)
+
+    "http://gravatar.com/avatar/#{hash}"
+  end
 end
