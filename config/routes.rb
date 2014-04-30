c016b17afe11121fdb7476da807f5fecab7575da
vudrok <jose@4web.mx>
2014-04-30 10:16:11 -0500

    Updated profile page and added scope status updates to it

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Treebook::Application.routes.draw do
   get 'feed', to: 'statuses#index', as: :feed
   root 'statuses#index'
 
+  get '/:id', to: 'profiles#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
